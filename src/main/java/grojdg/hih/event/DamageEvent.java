9f879d028a64e93c583c03ff09aa9966b2368a1c
xxk-i <jeremy.r.jacobson@gmail.com>
2023-06-16 15:23:43 -0400

    Random knockback applied to player on hih type damage

--- src/main/java/grojdg/hih/event/DamageEvent.java
+++ src/main/java/grojdg/hih/event/DamageEvent.java
@@ -14,8 +14,11 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 
 import java.util.Collection;
+import java.util.Random;
 
 public class DamageEvent {
+    private static Random rd = new Random();
+
     public static ActionResult OnDamage(DamageSource source, float amount, PlayerEntity player) {
         if(!source.getName().equals("hih")) {
             MinecraftServer server = player.getServer();
@@ -27,6 +30,9 @@ public class DamageEvent {
                     Registry<DamageType> dmgRegistry = server.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE);
                     RegistryEntry<DamageType> hihType = dmgRegistry.getEntry(dmgRegistry.get(new Identifier("hih")));
                     serverPlayer.damage(new DamageSource(hihType, null, null), 0.001f);
+
+                    // this strength number is roughly half that used in LivingEntity for tnt I think?
+                    serverPlayer.takeKnockback(0.2000000059604645, rd.nextBoolean() ? -1.0 * Math.random() : 1 * Math.random(), rd.nextBoolean() ? -1.0 * Math.random() : 1.0 * Math.random());
                     serverPlayer.setHealth(serverPlayer.getHealth() - amount);
                 }
 
